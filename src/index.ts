537e44ec8c0b99dc9139254e6ac32ca4fd9f6685
TatarianGit <lukipa82@gmail.com>
2024-05-14 01:17:29 +0200

    feat: add cors, express.json and urlencoded

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import express, { Express, Request, Response, Application } from "express";
+import express, { Request, Response, Application } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 //For env File
 dotenv.config();
@@ -7,10 +9,13 @@ dotenv.config();
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to Express & TypeScript Server");
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
 app.listen(port, () => {
-  console.log(`Server is Fire at http://localhost:${port}`);
+  console.log(
+    `============================\nServer is running at http://localhost:${port}`
+  );
 });
