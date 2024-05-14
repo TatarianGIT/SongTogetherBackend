05814b08c39d6460165bd4715865b4bea548b38e
TatarianGit <lukipa82@gmail.com>
2024-05-14 04:28:06 +0200

    feat: add auth route

---
+++ src/routes/auth.ts
@@ -0,0 +1,54 @@
+import { Router } from "express";
+import passport from "passport";
+import dotenv from "dotenv";
+
+export const authRoute = Router();
+
+dotenv.config();
+
+authRoute.get("/login/success", (req, res) => {
+  if (req.user) {
+    res.status(200).json({
+      success: true,
+      message: "successfull",
+      user: req.user,
+      cookies: req.cookies,
+    });
+  } else {
+    res.status(400).json({
+      success: false,
+      message: "unauthorized!",
+    });
+  }
+});
+
+authRoute.get("/login/failed", (req, res) => {
+  res.status(401).json({
+    success: false,
+    message: "failure",
+  });
+});
+
+authRoute.get("/logout", (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      console.log("LOGOUT ERROR ");
+      return next(err);
+    }
+    console.log("LOGOUT SUCCESS ");
+    res.redirect(process.env.CLIENT_URL);
+  });
+});
+
+authRoute.get(
+  "/discord",
+  passport.authenticate("discord", { scope: ["identify"] })
+);
+
+authRoute.get(
+  "/discord/callback",
+  passport.authenticate("discord", {
+    successRedirect: process.env.CLIENT_URL,
+    failureRedirect: "/login/failed",
+  })
+);
