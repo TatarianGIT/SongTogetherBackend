e75eee0d749b9a4da8836afdf82030be6816f5f8
TatarianGit <lukipa82@gmail.com>
2024-05-14 04:28:51 +0200

    feat: add User type

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export type User = {
+  user: {
+    id: string;
+    username: string;
+    avatar: string;
+    banner: boolean;
+    accent_color: number;
+    global_name: string;
+    banner_color: string;
+    email: string;
+    accessToken: string;
+    fetchedAt: string;
+  };
+};
