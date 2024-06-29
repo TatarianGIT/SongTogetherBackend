1aea5e72d63ea92949ca3bffff165907dbf67e29
TatarianGit <lukipa82@gmail.com>
2024-06-29 02:07:24 +0200

    feat: add function which return files with given extanstion

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -6,3 +6,17 @@ export const clearDirectory = async (directory: string) => {
     fs.unlinkSync(path.join(directory, file));
   }
 };
+
+export const findFilesWithExtension = async (
+  dir: string,
+  ext: string
+): Promise<{ filteredFiles: string[] } | null> => {
+  try {
+    const files = await fs.promises.readdir(dir);
+    const filteredFiles = files.filter((file) => path.extname(file) === ext);
+    return { filteredFiles };
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
