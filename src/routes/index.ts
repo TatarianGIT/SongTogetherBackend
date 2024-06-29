a6e8e98f2da7cdb0091a46462fb97132015cf637
TatarianGit <lukipa82@gmail.com>
2024-06-29 02:16:05 +0200

    feat: handle video streaming

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,31 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import AuthRoute from "./auth.js";
 import SongRoute from "./song.js";
 import { isAuthenticated } from "../middleware/isAuthenticated.js";
+import fs from "fs";
 
 const MainRoute = Router();
 
 MainRoute.use("/auth", AuthRoute);
 MainRoute.use("/song", isAuthenticated, SongRoute);
+
+//stream song
+MainRoute.use("", async (req: Request, res: Response) => {
+  var filePath = "." + req.url;
+
+  fs.readFile(filePath, function (error, content) {
+    if (error) {
+      console.log(error);
+      if (error.code == "ENOENT") {
+        res.json({ message: "File does not exist" }).status(400);
+      } else {
+        res.json({ message: "Unexpected error occured" }).status(400);
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": "application/vnd.apple.mpegurl" });
+      res.end(content, "utf-8");
+    }
+  });
+});
 
 export default MainRoute;
