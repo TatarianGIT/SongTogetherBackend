c0fb96fb584c9a39e981ade42b417217e2194610
TatarianGIT <lukipa82@gmail.com>
2024-08-06 00:27:26 +0200

    feat: create db queries for videos

---
+++ src/drizzle/videoServices.ts
@@ -0,0 +1,70 @@
+import { asc, eq, not } from "drizzle-orm";
+import { SongQueue } from "../types";
+import { db } from "./db";
+import { VideoList } from "./schema";
+
+export const addNewVideo = async (newVideo: SongQueue, isCurrent?: boolean) => {
+  try {
+    if (newVideo)
+      return await db
+        .insert(VideoList)
+        .values({
+          title: newVideo.title,
+          addedBy: newVideo.addedBy.global_name,
+          lengthSeconds: newVideo.lengthSeconds,
+          thumbnailUrl: newVideo.thumbnailUrl,
+          videoId: newVideo.videoId,
+          videoUrl: newVideo.videoUrl,
+          ...(isCurrent ? { type: "current" } : { type: "next" }),
+        })
+        .returning({
+          title: VideoList.title,
+          addedBy: VideoList.addedBy,
+          lengthSeconds: VideoList.lengthSeconds,
+          thumbnailUrl: VideoList.thumbnailUrl,
+          videoId: VideoList.videoId,
+          videoUrl: VideoList.videoId,
+        });
+  } catch (error) {
+    console.log("videoServieces, addNewVideo:", error);
+    return null;
+  }
+};
+
+export async function getNextVideoId() {
+  try {
+    const oldestVideo = await db
+      .select()
+      .from(VideoList)
+      .where(eq(VideoList.type, "next"))
+      .orderBy(asc(VideoList.timestamp))
+      .limit(1);
+    return oldestVideo[0].id;
+  } catch (error) {
+    console.log("videoServieces, getNextVideoId:", error);
+    return null;
+  }
+}
+
+export const getCurrentVideoId = async () => {
+  try {
+    const currentVideo = await db
+      .select()
+      .from(VideoList)
+      .where(eq(VideoList.type, "current"));
+
+    return currentVideo[0].id;
+  } catch (error) {
+    console.log("videoServieces, getCurrentVideoId:", error);
+    return null;
+  }
+};
+
+export const getNextQueue = async () => {
+  try {
+    return await db.select().from(VideoList);
+  } catch (error) {
+    console.log("videoServieces, getNextQueue:", error);
+    return null;
+  }
+};
