9fbf81fcaf39f7b73dbd87e834a8f6a984f11ed3
TatarianGIT <lukipa82@gmail.com>
2024-08-08 04:55:45 +0200

    feat: add session route to handle session actions

---
+++ src/routes/auth/session.ts
@@ -0,0 +1,56 @@
+import { Router } from "express";
+import { verifyRequestOrigin } from "lucia";
+import { lucia } from "../../config/auth.config.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const SessionRoute = Router();
+
+SessionRoute.use((req, res, next) => {
+  if (req.method === "GET") {
+    return next();
+  }
+  const originHeader = req.headers.origin ?? null;
+  const hostHeader = req.headers.host ?? null;
+
+  if (
+    !originHeader ||
+    !hostHeader ||
+    !verifyRequestOrigin(originHeader, [
+      hostHeader,
+      process.env.CLIENT_URL as string,
+    ])
+  ) {
+    return res.status(403).end();
+  }
+  return next();
+});
+
+SessionRoute.use(async (req, res, next) => {
+  const sessionId = lucia.readSessionCookie(req.headers.cookie ?? "");
+  if (!sessionId) {
+    res.locals.user = null;
+    res.locals.session = null;
+    return next();
+  }
+
+  const { session, user } = await lucia.validateSession(sessionId);
+  if (session && session.fresh) {
+    res.appendHeader(
+      "Set-Cookie",
+      lucia.createSessionCookie(session.id).serialize()
+    );
+  }
+  if (!session) {
+    res.appendHeader(
+      "Set-Cookie",
+      lucia.createBlankSessionCookie().serialize()
+    );
+  }
+  res.locals.session = session;
+  res.locals.user = user;
+  return next();
+});
+
+export default SessionRoute;
