0ebd00fb1181c41454ce3f9bb439bf64f5f4a1fb
TatarianGIT <lukipa82@gmail.com>
2024-08-08 04:54:27 +0200

    feat: initialize lucia auth with better-sqlite3 adapter and discord oauth

---
+++ src/config/auth.config.ts
@@ -0,0 +1,41 @@
+import { Lucia } from "lucia";
+import { Discord } from "arctic";
+import dotenv from "dotenv";
+import { db } from "../drizzle/db.js";
+import { BetterSqlite3Adapter } from "@lucia-auth/adapter-sqlite";
+
+import type { DatabaseUser } from "../types/index.js";
+
+dotenv.config();
+
+const adapter = new BetterSqlite3Adapter(db, {
+  user: "user",
+  session: "session",
+});
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    attributes: {
+      secure: process.env.NODE_ENV === "production",
+    },
+  },
+  getUserAttributes: (attributes) => {
+    return {
+      discordId: attributes.discord_id,
+      username: attributes.username,
+    };
+  },
+});
+
+export const discord = new Discord(
+  process.env.DISCORD_CLIENT_ID!,
+  process.env.DISCORD_CLIENT_SECRET!,
+  "http://localhost:3000/auth/discord/callback"
+);
+
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: Omit<DatabaseUser, "id">;
+  }
+}
