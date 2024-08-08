bfa0866cd51a0ceb295d76612413ce7fc159e2fe
TatarianGIT <lukipa82@gmail.com>
2024-08-08 04:54:01 +0200

    feat: add new types : DiscordUser, DatabaseUser

--- src/types/index.ts
+++ src/types/index.ts
@@ -55,3 +55,24 @@ export type SongQueue = {
   thumbnailUrl: string;
   addedBy: SocketUser;
 } | null;
+
+export type DiscordUser = {
+  id: string;
+  username: string;
+  avatar: string;
+  accent_color: number;
+  global_name: string;
+  banner_color: string;
+  email: string;
+};
+
+export type DatabaseUser = {
+  id: string;
+  discord_id: string;
+  username: string;
+  avatar: string;
+  accent_color: number;
+  global_name: string;
+  banner_color: string;
+  email: string;
+};
