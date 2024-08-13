0074ec3d47ca019ec59c81f138024bb7d41882ae
TatarianGIT <lukipa82@gmail.com>
2024-08-13 03:00:57 +0200

    refactor: update types after auth method change

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,52 +1,3 @@
-export type User = {
-  user: {
-    id: string;
-    username: string;
-    avatar: string;
-    banner: boolean;
-    accent_color: number;
-    global_name: string;
-    banner_color: string;
-    email: string;
-    accessToken: string;
-    fetchedAt: string;
-  };
-};
-
-export type IoUserResponse = {
-  passport: {
-    user: {
-      id: string;
-      username: string;
-      avatar: string;
-      discriminator: string;
-      public_flags: number;
-      flags: number;
-      banner: null | any;
-      accent_color: number;
-      global_name: string;
-      avatar_decoration_data: null | any;
-      banner_color: string;
-      clan: null | any;
-      mfa_enabled: boolean;
-      locale: string;
-      premium_type: number;
-      email: string;
-      verified: boolean;
-      provider: string;
-      accessToken: string;
-      fetchedAt: string;
-    };
-  };
-};
-
-export type SocketUser = {
-  id: string;
-  global_name: string;
-  avatar: string;
-  banner_color: string;
-};
-
 export type SongQueue = {
   videoUrl: string;
   videoId: string;
@@ -75,4 +26,11 @@ export type DatabaseUser = {
   global_name: string;
   banner_color: string;
   email: string;
+};
+
+export type SocketUser = {
+  discord_id: string;
+  avatar: string;
+  global_name: string;
+  banner_color: string;
 };
