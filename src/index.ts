ad6064b7b80d26c77401d9bfd9a93257b6f467e9
TatarianGIT <lukipa82@gmail.com>
2024-10-08 22:24:05 +0200

    refactor: update starting file

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 import cookieSession from "cookie-session";
 import morgan from "morgan";
 import { createServer } from "https";
-import fs from 'fs';
+import fs from "fs";
 import { configureSocketIO } from "./socketio/socket.js";
 import MainRoute from "./routes/index.js";
 
@@ -13,7 +13,8 @@ dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 const app: Application = express();
 const PORT = process.env.PORT;
-const HOST = process.env.HOST;
+const HOST_URL = process.env.HOST_URL;
+const NODE_ENV = process.env.NODE_ENV;
 
 const sslKey = fs.readFileSync("/home/luke/selfsigned.key", "utf8");
 const sslCert = fs.readFileSync("/home/luke/selfsigned.crt", "utf8");
@@ -46,6 +47,7 @@ configureSocketIO(httpsServer);
 
 httpsServer.listen(PORT, () => {
   console.log(
-    `Running ${process.env.NODE_ENV} build of SongTogether\nServer is running at http://${HOST}:${PORT}\n\n`
+    `Running ${NODE_ENV} build of SongTogether\n
+    Server is running at https://${HOST_URL}:${PORT}\n\n`
   );
 });
